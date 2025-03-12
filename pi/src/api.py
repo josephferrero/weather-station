c5b22807ed779b401e3f4dfd662c5c333a876433
josephferrero <joe.ferrero@philips.com>
2025-03-12 15:49:57 -0700

    add exceptions to post

--- pi/src/api.py
+++ pi/src/api.py
@@ -4,7 +4,7 @@ import sensor
 
 def send_reading(data):
     data = sensor.read_bme680()
-
+    response = ""
     try:
         response = requests.post(config.API_URL + "/readings", json=data, timeout=1.5)
     except requests.Timeout:
@@ -13,6 +13,6 @@ def send_reading(data):
     except requests.ConnectionError:
         print("there was connection error")
         pass
-    
+
     print(response)
     print("Response:", response.json())
