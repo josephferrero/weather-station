a919ab28193ff775ef783e2a1d01408d56904d77
josephferrero <joe.ferrero@philips.com>
2025-03-12 15:32:07 -0700

    fix syntax

--- pi/src/api.py
+++ pi/src/api.py
@@ -3,7 +3,7 @@ import config
 import sensor
 
 def send_reading(data):
-    data = sensor.read_bme680
+    data = sensor.read_bme680()
     response = requests.post(config.API_URL + "/readings", json=data)
     print(response)
     print("Response:", response.json())
