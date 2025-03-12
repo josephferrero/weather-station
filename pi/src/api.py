27916aabe702785b3db5881a00f9acbf4d8141cb
josephferrero <joe.ferrero@philips.com>
2025-03-12 15:53:21 -0700

    use else in try block

--- pi/src/api.py
+++ pi/src/api.py
@@ -4,15 +4,15 @@ import sensor
 
 def send_reading(data):
     data = sensor.read_bme680()
-    response = {}
     try:
         response = requests.post(config.API_URL + "/readings", json=data, timeout=1.5)
     except requests.Timeout:
         print("the request timed out")
         pass
     except requests.ConnectionError:
-        print("there was connection error")
+        print("there a was connection error")
         pass
-
-    print(response)
-    print("Response:", response.json())
+    else:
+        print(response)
+        print("Response:", response.json())
+        
