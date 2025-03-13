7fb3ad712861b4d32aef0b96afa5785f26e721fc
josephferrero <joe.ferrero@philips.com>
2025-03-13 16:54:20 -0700

    add more logging

--- api/cmd/server/server.go
+++ api/cmd/server/server.go
@@ -10,11 +10,10 @@ import (
 )
 
 func StartServer() {
-	mux := http.NewServeMux()
-
 	myFigure := figure.NewFigure("Weather Station API", "", true)
 	myFigure.Print()
 
+	mux := http.NewServeMux()
 	mux.HandleFunc("/readings", handlers.HandleWeatherReading())
 
 	fmt.Println("REST API running on port 8080...")
